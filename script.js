742af1606b430d8a6474c259b79ecafd4a4f768c
VedaY <veda.yakkali3@gmail.com>
2024-02-12 13:39:21 -0500

    Fixed Problem With User Input with HTML Tags

--- script.js
+++ script.js
@@ -34,8 +34,9 @@ const generateResponse = (incomingChatLi) => {
 const createChatLi = (message, className) => {
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", className);
-    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
     chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
     return chatLi;
 }
 
